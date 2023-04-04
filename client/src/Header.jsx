8d4710eca1e0e27512484edf990d56cd6a217c0d
Mugdin12 <mugdin.krnjic20@gmail.com>
2023-04-04 15:23:30 +0200

    Edit login page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -17,8 +17,8 @@ export default function Header() {
             strokeLinejoin='round'
             d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
           />
-          <span className='font-bold text-xl'>airsin</span>
         </svg>
+        <span className='font-bold text-xl'>AirSin</span>
       </a>
       <div className='flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
         <div>Anywhere</div>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,14 @@
 export default function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <div className='mt-4 grow flex items-center justify-around'>
+      <div className='mb-64'>
+        <h1 className='text-4xl text-center mb-4'>Login</h1>
+        <form action='' className='max-w-md mx-auto'>
+          <input type='email' placeholder='your@gmail.com' />
+          <input type='password' placeholder='password' />
+          <button className='primary'>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
