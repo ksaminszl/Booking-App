a0ba7fa50f7b4021ea53d6c56a1e03f8477f099e
Mugdin12 <mugdin111@gmail.com>
2023-04-10 15:33:23 +0200

    Fix register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import { Route, Routes } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/register'} element={<RegisterPage />} />
       </Route>
     </Routes>
   );
