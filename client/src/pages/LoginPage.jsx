441a01c9e11e0ab0f87681db97219e18fd87be38
Mugdin12 <mugdin.krnjic20@gmail.com>
2023-04-10 15:00:34 +0200

    Add link in login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 export default function LoginPage() {
   return (
     <div className='mt-4 grow flex items-center justify-around'>
@@ -7,6 +8,12 @@ export default function LoginPage() {
           <input type='email' placeholder='your@gmail.com' />
           <input type='password' placeholder='password' />
           <button className='primary'>Login</button>
+          <div className='text-center py-2 text-gray-500'>
+            Don't have an account yet?{' '}
+            <Link className='underline text-black' to={'/register'}>
+              Register now
+            </Link>
+          </div>
         </form>
       </div>
     </div>
